283b1661dd271afbd4117994a853061052832a0a
BenZheng19761976 <benzheng1976@gmail.com>
2020-09-03 17:49:05 +1000

    Update Unit Test Example

--- reactcode/tests/App.test.tsx
+++ reactcode/tests/App.test.tsx
@@ -14,4 +14,22 @@ it('App is rendered', () => {
     // Verify seven movie tab content
      expect(appNode.findDOMNode('div01').findDOMNode('sevenTitle01')).toEqual('Gold Digger');
 
+     expect(appNode.findDOMNode('div02').findDOMNode('sevenTitle02')).toEqual('Liar');
+
+     expect(appNode.findDOMNode('div03').findDOMNode('sevenTitle03')).toEqual('Little Women: LA');
+
+     expect(appNode.findDOMNode('div04').findDOMNode('sevenTitle04')).toEqual('My Kitchen Rules');
+
+     expect(appNode.findDOMNode('div05').findDOMNode('sevenTitle05')).toEqual('Pooch Perfect');
+
+     expect(appNode.findDOMNode('div06').findDOMNode('sevenTitle06')).toEqual('Revolution');
+
+     expect(appNode.findDOMNode('div07').findDOMNode('sevenTitle07')).toEqual('Smallville');
+
+     expect(appNode.findDOMNode('div08').findDOMNode('sevenTitle08')).toEqual('The Good Doctor');
+
+     expect(appNode.findDOMNode('div09').findDOMNode('sevenTitle09')).toEqual('The Resident');
+
+     expect(appNode.findDOMNode('div10').findDOMNode('sevenTitle10')).toEqual('Transformed');
+
 });
